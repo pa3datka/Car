6a1735b7782e37ed2bbd2edb7d2e6b13e92a8e58
pa3datka <63630679+pa3datka@users.noreply.github.com>
2021-01-08 19:08:37 +0300

    change ClientController
    change ClientController

--- app/Http/Controllers/TestTaskControllers/Api/ClientController.php
+++ app/Http/Controllers/TestTaskControllers/Api/ClientController.php
@@ -18,11 +18,14 @@ class ClientController extends Controller
     public function result($query)
     {
         $query = trim($query);
+        $result = '';
         if ($query) {
-            $url = "https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/ $query ?format=json";
+            $url = "https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/$query?format=json";
             $client = new HttpClient();
             $result = $client->request($url);
-            return response()->json($result);
+        } else {
+            $result = 'Invalid VIN';
         }
+        return response()->json($result);
     }
 }
